a12fade8b7ebeeaa20230f81897cdb7f29d74a6c
kksvt <kksvt>
2025-11-22 14:42:51 +0100

    Initial Commit

---
+++ ios-todo/ios_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_todoApp.swift
+//  ios-todo
+//
+//  Created by user279407 on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
